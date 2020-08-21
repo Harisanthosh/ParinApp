13c9ebf25d4a9ef1c8e13467cfba7f05f8c72c94
Harisanthosh <harisanthosh94@gmail.com>
2020-08-21 05:39:25 +0200

    Data collector when temp exceeds 84.3

--- application.py
+++ application.py
@@ -37,27 +37,39 @@ app.layout = html.Div(style={'textAlign': 'center'},children=[
      
 ])
              
-@app.callback(Output('example-graph', 'figure'),[Input('interval-component', 'n_intervals')])
+@app.callback([Output('example-graph', 'figure'),Output('live-update-text', 'children')],[Input('interval-component', 'n_intervals')])
 def update_layout(n):
     r = requests.get('https://api.parin.io/sensor/4221', headers=headers)
     
     #dict_r = r.json()
     temp_arr = []
     timestamp_arr = []
+    hightemp_arr = []
+    hightemp_timestamp = []
+    high_val_alarm = 0
+    val_text = ''
     
     dict_val = json.loads(r.text)
     for val in dict_val:
         temp_arr.append(val['temperature'])
         timestamp_arr.append(val['timestamp'])
+        if(float(val['temperature']) > 84.3):
+            high_val_alarm = 1
+            hightemp_arr.append(val['temperature'])
+            hightemp_timestamp.append(val['timestamp'])
+            val_text = f'The temperature {hightemp_arr[0]} has execeeded the threshold at {hightemp_timestamp[0]}'
     
     df = pd.DataFrame({
         "Temp": temp_arr,
         "Date": timestamp_arr
     })
     
+    if(high_val_alarm == 1):
+        df.to_csv('Temperature_observations.csv',mode='a', header=False)
+    
     fig = px.line(df, x="Date", y="Temp")
     
-    return fig
+    return fig, val_text
 
 application = app.server
 
